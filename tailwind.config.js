ab76bb4d484f9f4b93ee1d3180c3611d2c066080
mg97-2022 <mohamedgamalsaied1997@gmail.com>
2023-05-14 00:14:47 +0300

    general tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#232862",
+        primaryLight: "#5D3D8C",
+        primaryVeryLight: "#393e72",
+        secondary: "#FEE528",
+        secondaryLight: "#F2F5F8",
+        secondaryVeryLight: "#fffbed",
+        secondaryOverlay: "#FED700", // there is problem with this color
+        greyLightFirst: "#F6F3F4",
+        greyLightSecond: "#9B9B9B",
+        greyDark: "#333E48",
       },
+      // fontSize: {
+      //   "15px": "15px",
+      // },
     },
   },
-  plugins: [],
-}
+  // plugins: [],
+};
