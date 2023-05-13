0ab805679ac0dc38009fbcffa83f6e5d88536a3e
mg97-2022 <mohamedgamalsaied1997@gmail.com>
2023-05-14 00:17:10 +0300

    language context

---
+++ src/context/languageContext.js
@@ -0,0 +1,24 @@
+import React, { useState, createContext } from "react";
+import LangUtils from "../utils/language";
+
+const LanguageContext = createContext();
+
+export const LanguageContextProvider = ({ children }) => {
+  const [language, setLanguage] = useState(LangUtils.langObj.ENGLISH);
+
+  const changeLanguageHandler = (changedLanguage) => {
+    setLanguage(changedLanguage);
+  };
+
+  const store = {
+    language,
+    changeLanguage: changeLanguageHandler,
+  };
+  return (
+    <LanguageContext.Provider value={store}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export default LanguageContext;
