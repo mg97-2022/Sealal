82140ed2f8d1266e572a528a21628f9511cdae0d
mg97-2022 <mohamedgamalsaied1997@gmail.com>
2023-05-15 01:00:10 +0300

    delivery partner duplicated images

--- src/components/DeliveryPartner.jsx
+++ src/components/DeliveryPartner.jsx
@@ -23,9 +23,17 @@ const DeliveryPartner = () => {
           "/image 9.png",
           "/image 10.png",
           "/image 10.png",
+          "/image 1.png",
+          "/image 3.png",
+          "/image 4.png",
+          "/image 7.png",
+          "/image 8.png",
+          "/image 9.png",
+          "/image 10.png",
+          "/image 10.png",
         ].map((img, index) => (
           <div key={index} className="flex items-center justify-center">
-            <Image src={img} alt="img" width="80" height="80" />
+            <Image className="mx-auto" src={img} alt="img" width="80" height="80" />
           </div>
         ))}
       </CustomCarousel>
