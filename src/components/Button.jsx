95135db906c4ceca3ab5ddb7a39d4ede5d43747c
mg97-2022 <mohamedgamalsaied1997@gmail.com>
2023-05-15 00:57:17 +0300

    updated button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,13 @@
 import LangUtils from "../utils/language";
 
-const Button = ({ button, onClick, className, type }) => {
+const Button = ({ button, onClick, className, type, buttonColor }) => {
   return (
     <button
       type={type || "button"}
       onClick={onClick}
-      className={`${className} bg-primary px-20 py-3 rounded-full text-white font-medium leading-[36px]`}
+      className={`${className} ${
+        buttonColor ? buttonColor : "bg-primary"
+      } px-20 py-3 rounded-full text-white font-medium leading-[36px]`}
     >
       {LangUtils.FetchWord(button)}
     </button>
