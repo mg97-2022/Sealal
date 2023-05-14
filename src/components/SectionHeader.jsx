8cc1185f05710db06603e868c951443de707cb84
mg97-2022 <mohamedgamalsaied1997@gmail.com>
2023-05-15 00:58:44 +0300

    updated section header component

--- src/components/SectionHeader.jsx
+++ src/components/SectionHeader.jsx
@@ -1,12 +1,16 @@
+import LangUtils from "../utils/language";
+
 const SectionHeader = ({ heading, description, showSideLines }) => {
   return (
     <div className="my-10 text-center">
       <div className="flex items-center justify-center">
-        {showSideLines && <span className="h-[1px] w-[150px] bg-primary"/>}
-        <h3 className="px-11  text-[22px]  text-primary font-medium leading-[36px]">{heading}</h3>
-        {showSideLines && <span className="h-[1px] w-[150px] bg-primary"/>}
+        {showSideLines && <span className="h-[1px] w-[150px] bg-primary" />}
+        <h3 className="px-11  text-[22px]  text-primary font-medium leading-[36px]">
+          {LangUtils.FetchWord(heading)}
+        </h3>
+        {showSideLines && <span className="h-[1px] w-[150px] bg-primary" />}
       </div>
-      {description && <p>{description}</p>}
+      {description && <p>{LangUtils.FetchWord(description)}</p>}
     </div>
   );
 };
