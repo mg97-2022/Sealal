2432a5f74311b4d95a7de27711d56c5a6dd0c90d
mg97-2022 <mohamedgamalsaied1997@gmail.com>
2023-05-17 01:04:28 +0300

    custom select input field

---
+++ src/components/SelectInputField.jsx
@@ -0,0 +1,45 @@
+const SelectInputField = ({
+  children,
+  containerClassName,
+  label,
+  name,
+  type,
+  placeholder,
+  register,
+  errorMessage,
+  required = true,
+  inputClassName,
+  input,
+}) => {
+  return (
+    <div className={`flex flex-col justify-between min-w-[110px] w-full ${containerClassName}`}>
+      <label
+        className={`mb-3 first-letter-capitalize text-sm ${
+          errorMessage && "text-xs text-red-500"
+        }`}
+        htmlFor={name}
+      >
+        {label}
+        {!!errorMessage && (
+          <span>{errorMessage}</span>
+        )}
+      </label>
+      <select
+        className={`bg-transparent rounded-2 rounded-[10px] border border-black outline-none h-10 px-2 text-xs placeholder:text-greyLightFourth ${inputClassName} ${
+          errorMessage && "border-red-500"
+        }`}
+        id={name}
+        type={type || "text"}
+        {...register(name, {
+          required: required && ` is required *`,
+        })}
+        placeholder={placeholder || label}
+        {...input}
+      >
+        {children}
+      </select>
+    </div>
+  );
+};
+
+export default SelectInputField;
