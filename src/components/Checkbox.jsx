371407f570ed6eee512976824ef63516e66aef74
mg97-2022 <mohamedgamalsaied1997@gmail.com>
2023-05-22 02:39:16 +0300

    enhanced checkbox

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -12,13 +12,15 @@ const Checkbox = ({
     <div className={`flex items-center gap-2 ${containerClassName}`}>
       <input
         type="checkbox"
-        className={`rounded-2 rounded-[10px] border border-greyLightFifth outline-none w-6 h-6  px-2 accent-secondary ${inputClassName}`}
+        className={`cursor-pointer rounded-2 rounded-[10px] border border-greyLightFifth outline-none w-6 h-6  px-2 accent-secondary ${inputClassName}`}
         id={name}
         {...register(name)}
         {...input}
       />
       <label
-        className={"first-letter:capitalize text-lg text-greyDark"}
+        className={
+          "cursor-pointer first-letter:capitalize text-lg text-greyDark"
+        }
         htmlFor={name}
       >
         {LangUtils.FetchWord(label)}
