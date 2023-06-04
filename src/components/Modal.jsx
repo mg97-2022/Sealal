c36b78bef8fabd32ad11c018d1750d3022915b9e
mg97-2022 <mohamedgamalsaied1997@gmail.com>
2023-06-04 14:58:16 +0300

    use Framer motion with modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,26 @@
 import { Fragment, useEffect } from "react";
+import { motion } from "framer-motion";
+
+const dropIn = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      type: "spring",
+      damping: 25,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "-100vh",
+    opacity: 0,
+  },
+};
 
 const Modal = ({ children, onClose, disableScroll = true }) => {
   useEffect(() => {
@@ -12,13 +34,24 @@ const Modal = ({ children, onClose, disableScroll = true }) => {
 
   return (
     <Fragment>
-      <div
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.3 }}
         className="fixed w-[100vw] h-[100vh] top-0 left-0 bg-[#000000bf] z-[990]"
         onClick={onClose}
       />
-      <div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] px-5 rounded-[10px] box-shadow bg-white w-[700px] lg:w-[900px] h-[250px] lg:h-[340px] max-w-[90%] flex items-center justify-center z-[999]">
+      <motion.div
+        variants={dropIn}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        transition={{ duration: 0.5 }}
+        className="fixed px-5 rounded-[10px] box-shadow bg-white w-[700px] lg:w-[900px] h-[250px] lg:h-[340px] max-w-[90%] flex items-center justify-center z-[999]"
+      >
         {children}
-      </div>
+      </motion.div>
     </Fragment>
   );
 };
--- src/components/TrackShipment/ShipmentFailed.jsx
+++ src/components/TrackShipment/ShipmentFailed.jsx
@@ -2,6 +2,7 @@ import LangUtils from "@/utils/language";
 import SquareButton from "../SquareButton";
 import { Fragment, useState } from "react";
 import Modal from "../Modal";
+import { AnimatePresence } from "framer-motion";
 
 const ShipmentFailed = () => {
   const [openModal, setOpenModal] = useState(false);
@@ -12,32 +13,34 @@ const ShipmentFailed = () => {
 
   return (
     <Fragment>
-      {openModal && (
-        <Modal onClose={closeModalHandler}>
-          <div>
-            <h6 className="mb-10 md:mb-12 lg:mb-[70px] text-center font-semibold first-letter:capitalize lg:text-lg text-primary">
-              {LangUtils.FetchWord("refundToSeller")}
-            </h6>
-            <div className="flex items-center justify-center flex-wrap gap-2">
-              <SquareButton
-                onClick={() => {}}
-                bgColor="bg-primary"
-                className="h-[45px] md:h-[50px] w-[280px] lg:h-[60px] md:w-[302px]"
-              >
-                receiptInWarehouse
-              </SquareButton>
-              <SquareButton
-                onClick={openModalHandler}
-                className="h-[45px] md:h-[50px] w-[280px] lg:h-[60px] md:w-[302px] border border-primary"
-                bgColor="transparent"
-                textColor="text-primary"
-              >
-                sendToSeller
-              </SquareButton>
+      <AnimatePresence initial={true} mode="wait">
+        {openModal && (
+          <Modal onClose={closeModalHandler}>
+            <div>
+              <h6 className="mb-10 md:mb-12 lg:mb-[70px] text-center font-semibold first-letter:capitalize lg:text-lg text-primary">
+                {LangUtils.FetchWord("refundToSeller")}
+              </h6>
+              <div className="flex items-center justify-center flex-wrap gap-2">
+                <SquareButton
+                  onClick={() => {}}
+                  bgColor="bg-primary"
+                  className="h-[45px] md:h-[50px] w-[280px] lg:h-[60px] md:w-[302px]"
+                >
+                  receiptInWarehouse
+                </SquareButton>
+                <SquareButton
+                  onClick={openModalHandler}
+                  className="h-[45px] md:h-[50px] w-[280px] lg:h-[60px] md:w-[302px] border border-primary"
+                  bgColor="transparent"
+                  textColor="text-primary"
+                >
+                  sendToSeller
+                </SquareButton>
+              </div>
             </div>
-          </div>
-        </Modal>
-      )}
+          </Modal>
+        )}
+      </AnimatePresence>
       <div>
         <p className="text-[#2E2E2E] text-sm mb-[66px] text-center">
           {LangUtils.FetchWord("unableReason")}
